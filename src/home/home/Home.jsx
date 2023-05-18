4b72c8c09ea49d64ff404f2d01023c22e6ea5c6d
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-18 18:03:51 +0600

    completed the gallery section

--- src/home/home/Home.jsx
+++ src/home/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../banner/Banner';
+import Galaries from '../galaries/Galaries';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <Galaries></Galaries>
         </div>
     );
 };
